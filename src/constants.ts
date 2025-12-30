4ae981e78882544418e319ceab1ba720208ae9d1
Ross Libby <rosslibby92@gmail.com>
2025-12-29 20:46:42 -0500

    defined serviceconstants

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const BITRATES = [
+  0, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 0
+] as const;
+
+export const SAMPLERATES = [44100, 48000, 32000, 0] as const;
+
+export const SUPPORTED_MIME_TYPES = [
+  'audio/mpeg', 'audio/mp3', 'application/octet-stream'
+];
